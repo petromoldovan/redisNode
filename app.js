cd593f1227be36c4783ee35555474336d0aa2cd9
petro moldovan <petro.moldovan@gmail.com>
2017-09-16 12:03:38 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const morgan = require('morgan');
+const redis = require('redis');
+
+const router = require('./router');
+
+//redis client
+const client = redis.createClient();
+client.on('connect', function() {
+  console.log('redis connected');
+})
+
+//set port
+const PORT = 3000;
+
+//initialize app
+const app = express();
+app.use(bodyParser.json({type: '*/*'}));
+
+//log incoming request
+app.use(morgan('combined'));
+
+//set router
+router(app);
+
+//listen for requests
+app.listen(PORT, function(){
+  console.log('Server listening on:', PORT)
+})
